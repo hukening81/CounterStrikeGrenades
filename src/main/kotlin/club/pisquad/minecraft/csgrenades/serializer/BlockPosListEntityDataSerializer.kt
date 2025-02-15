f0646a66e44e89c24fad84a70b6f19c5218f1071
hukening <kening.hu@outlook.com>
2025-02-15 10:11:57 +0800

    fix:(BlockPosListEntityDataSerializer): crash when serializing BlockPosList EntityData

--- src/main/kotlin/club/pisquad/minecraft/csgrenades/serializer/BlockPosListEntityDataSerializer.kt
+++ src/main/kotlin/club/pisquad/minecraft/csgrenades/serializer/BlockPosListEntityDataSerializer.kt
@@ -1,19 +1,24 @@
 package club.pisquad.minecraft.csgrenades.serializer
 
-import club.pisquad.minecraft.csgrenades.network.serializer.BlockPosSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.IntArraySerializer
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 import net.minecraft.core.BlockPos
 import net.minecraft.network.FriendlyByteBuf
 import net.minecraft.network.syncher.EntityDataSerializer
 
-class BlockPosListEntityDataSerializer : EntityDataSerializer<List<BlockPos>> {
-    override fun write(pBuffer: FriendlyByteBuf, pValue: List<BlockPos>) {
-        pBuffer.writeUtf(Json.encodeToString(ListSerializer(BlockPosSerializer()), pValue))
+class BlockPosListEntityDataSerializer : EntityDataSerializer<List<@Serializable BlockPos>> {
+    override fun write(pBuffer: FriendlyByteBuf, pValue: List<@Serializable BlockPos>) {
+        val result = mutableListOf<IntArray>()
+        pValue.forEach { result.add(arrayOf(it.x, it.y, it.z).toIntArray()) }
+        pBuffer.writeUtf(Json.encodeToString(ListSerializer(IntArraySerializer()), result))
     }
 
     override fun read(pBuffer: FriendlyByteBuf): List<BlockPos> {
-        return Json.decodeFromString<List<BlockPos>>(pBuffer.readUtf())
+
+        val result = Json.decodeFromString<List<IntArray>>(pBuffer.readUtf())
+        return result.map { BlockPos(it[0], it[1], it[2]) }
     }
 
     override fun copy(pValue: List<BlockPos>): List<BlockPos> {
