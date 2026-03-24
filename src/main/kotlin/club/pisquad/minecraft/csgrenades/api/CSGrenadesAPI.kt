bd7ef170d7765e345535e89cd72d84760e49cbd6
Kening Hu <hukening81@gmail.com>
2026-03-25 00:37:35 +0800

    refactor(CSGrenadeAPI): use a more unified way to do things

--- src/main/kotlin/club/pisquad/minecraft/csgrenades/api/CSGrenadesAPI.kt
+++ src/main/kotlin/club/pisquad/minecraft/csgrenades/api/CSGrenadesAPI.kt
@@ -1,51 +1,112 @@
 package club.pisquad.minecraft.csgrenades.api
 
-import net.minecraft.world.entity.player.Player
+import club.pisquad.minecraft.csgrenades.GrenadeType
+import club.pisquad.minecraft.csgrenades.ModLogger
+import club.pisquad.minecraft.csgrenades.core.entity.CounterStrikeGrenadeEntity
+import club.pisquad.minecraft.csgrenades.core.item.CounterStrikeGrenadeItem
+import club.pisquad.minecraft.csgrenades.getEntity
+import club.pisquad.minecraft.csgrenades.network.serializer.UUIDSerializer
+import club.pisquad.minecraft.csgrenades.network.serializer.Vec3Serializer
+import kotlinx.serialization.Serializable
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.phys.Vec3
 import java.util.*
-import java.util.concurrent.ConcurrentHashMap
 
-/**
- * 提供了与CS手榴弹模组交互的公共API。
- */
 object CSGrenadesAPI {
-
-    // 存储被闪光弹致盲的玩家UUID及其剩余的致盲时间（tick）
-    private val flashedPlayers: ConcurrentHashMap<UUID, Int> = ConcurrentHashMap()
-
     /**
-     * 检查给定的玩家是否当前被CS手榴弹模组的闪光弹致盲。
-     * @param player 要检查的玩家。
-     * @return 如果玩家被致盲，则返回 true；否则返回 false。
+     * Spawn a grenade to the world
+     *
+     * @param context
+     * @return spanwed entity if success
      */
-    @JvmStatic
-    fun isPlayerFlashed(player: Player): Boolean =
-        flashedPlayers.containsKey(player.uuid) && (flashedPlayers[player.uuid] ?: 0) > 0
+    fun spawnGrenade(
+        owner: ServerPlayer,
+        context: GrenadeSpawnContext,
+        removeItem: Boolean = true
+    ): CounterStrikeGrenadeEntity? {
+        val level = owner.level() as ServerLevel
+        val entityType = context.grenadeType.getEntity()
+        val entity = entityType.create(level) ?: return null
+        entity.initialize(context.ownerUuid, context.position, context.velocity)
 
-    /**
-     * 内部方法：设置玩家的闪光弹致盲状态和持续时间。
-     * 仅供CS手榴弹模组内部使用。
-     * @param player 被致盲的玩家。
-     * @param durationTicks 致盲持续时间，单位是游戏刻 (tick)。
-     */
-    internal fun setPlayerFlashed(player: Player, durationTicks: Int) {
-        if (durationTicks > 0) {
-            flashedPlayers[player.uuid] = durationTicks
-        } else {
-            flashedPlayers.remove(player.uuid)
+        ModLogger.info("Spawning ${context.grenadeType} entity at ${context.position} with velocity ${context.velocity.length()} blocks per tick")
+        level.addFreshEntity(entity)
+
+        if (removeItem) {
+            Player.removeGrenadeFromInventory(owner, context.grenadeType)
         }
+        return entity
     }
 
-    /**
-     * 内部方法：在每个服务器tick更新玩家的致盲状态。
-     * 仅供CS手榴弹模组内部使用。
-     */
-    internal fun onServerTick() {
-        flashedPlayers.forEach { (uuid, duration) ->
-            if (duration <= 1) { // 如果持续时间小于等于1，则移除
-                flashedPlayers.remove(uuid)
+    object Player {
+        fun removeGrenadeFromInventory(player: ServerPlayer, grenadeType: GrenadeType): Boolean {
+            val item = player.mainHandItem.item
+            if (item is CounterStrikeGrenadeItem && item.grenadeType == grenadeType) {
+                player.mainHandItem.count--
             } else {
-                flashedPlayers[uuid] = duration - 1
+                player.inventory.items.forEach {
+                    val item = it.item
+                    if (item is CounterStrikeGrenadeItem && item.grenadeType == grenadeType) {
+                        it.count--
+                        return true
+                    }
+                }
             }
+            return false
         }
     }
 }
+
+@Serializable
+data class GrenadeSpawnContext(
+    val grenadeType: GrenadeType,
+    @Serializable(with = UUIDSerializer::class) val ownerUuid: UUID,
+    @Serializable(with = Vec3Serializer::class) val position: Vec3,
+    @Serializable(with = Vec3Serializer::class) val velocity: Vec3,
+)
+///**
+// * 提供了与CS手榴弹模组交互的公共API。
+// */
+//object CSGrenadesAPI {
+//
+//    // 存储被闪光弹致盲的玩家UUID及其剩余的致盲时间（tick）
+//    private val flashedPlayers: ConcurrentHashMap<UUID, Int> = ConcurrentHashMap()
+//
+//    /**
+//     * 检查给定的玩家是否当前被CS手榴弹模组的闪光弹致盲。
+//     * @param player 要检查的玩家。
+//     * @return 如果玩家被致盲，则返回 true；否则返回 false。
+//     */
+//    @JvmStatic
+//    fun isPlayerFlashed(player: Player): Boolean =
+//        flashedPlayers.containsKey(player.uuid) && (flashedPlayers[player.uuid] ?: 0) > 0
+//
+//    /**
+//     * 内部方法：设置玩家的闪光弹致盲状态和持续时间。
+//     * 仅供CS手榴弹模组内部使用。
+//     * @param player 被致盲的玩家。
+//     * @param durationTicks 致盲持续时间，单位是游戏刻 (tick)。
+//     */
+//    internal fun setPlayerFlashed(player: Player, durationTicks: Int) {
+//        if (durationTicks > 0) {
+//            flashedPlayers[player.uuid] = durationTicks
+//        } else {
+//            flashedPlayers.remove(player.uuid)
+//        }
+//    }
+//
+//    /**
+//     * 内部方法：在每个服务器tick更新玩家的致盲状态。
+//     * 仅供CS手榴弹模组内部使用。
+//     */
+//    internal fun onServerTick() {
+//        flashedPlayers.forEach { (uuid, duration) ->
+//            if (duration <= 1) { // 如果持续时间小于等于1，则移除
+//                flashedPlayers.remove(uuid)
+//            } else {
+//                flashedPlayers[uuid] = duration - 1
+//            }
+//        }
+//    }
+//}
--- src/main/kotlin/club/pisquad/minecraft/csgrenades/network/message/ClientGrenadeThrowMessage.kt
+++ src/main/kotlin/club/pisquad/minecraft/csgrenades/network/message/ClientGrenadeThrowMessage.kt
@@ -1,62 +1,27 @@
 package club.pisquad.minecraft.csgrenades.network.message
 
-import club.pisquad.minecraft.csgrenades.GrenadeType
-import club.pisquad.minecraft.csgrenades.ModLogger
+import club.pisquad.minecraft.csgrenades.api.CSGrenadesAPI
+import club.pisquad.minecraft.csgrenades.api.GrenadeSpawnContext
 import club.pisquad.minecraft.csgrenades.client.input.InputState.calculateGrenadeSpeed
 import club.pisquad.minecraft.csgrenades.core.item.CounterStrikeGrenadeItem
-import club.pisquad.minecraft.csgrenades.getEntity
 import club.pisquad.minecraft.csgrenades.getShootOrigin
 import club.pisquad.minecraft.csgrenades.network.CsGrenadeMessageHandler
-import club.pisquad.minecraft.csgrenades.network.serializer.UUIDSerializer
-import club.pisquad.minecraft.csgrenades.network.serializer.Vec3Serializer
 import kotlinx.serialization.Serializable
 import net.minecraft.client.Minecraft
-import net.minecraft.server.level.ServerPlayer
-import net.minecraft.world.level.Level
-import net.minecraft.world.phys.Vec3
 import net.minecraftforge.network.NetworkEvent
-import java.util.*
 import java.util.function.Supplier
 
 @Serializable
 data class ClientGrenadeThrowMessage(
-    @Serializable(with = UUIDSerializer::class) val ownerUuid: UUID,
-    @Serializable(with = Vec3Serializer::class) val position: Vec3,
-    @Serializable(with = Vec3Serializer::class) val velocity: Vec3,
-    val grenadeType: GrenadeType,
+    val context: GrenadeSpawnContext
 ) {
     companion object : CsGrenadeMessageHandler<ClientGrenadeThrowMessage>(ClientGrenadeThrowMessage::class) {
         override fun handler(msg: ClientGrenadeThrowMessage, ctx: Supplier<NetworkEvent.Context>) {
             val context = ctx.get()
             val player = context.sender ?: return
 
-            if (!player.isCreative) {
-                removeGrenadeFromInventory(player, msg.grenadeType)
-            }
-            spawnGrenadeEntity(player.level(), msg)
-        }
-
-        private fun removeGrenadeFromInventory(player: ServerPlayer, grenadeType: GrenadeType) {
-            val item = player.mainHandItem.item
-            if (item is CounterStrikeGrenadeItem && item.grenadeType == grenadeType) {
-                player.mainHandItem.count--
-            } else {
-                player.inventory.items.forEach {
-                    val item = it.item
-                    if (item is CounterStrikeGrenadeItem && item.grenadeType == grenadeType) {
-                        it.count--
-                        return
-                    }
-                }
-            }
-        }
-
-        private fun spawnGrenadeEntity(level: Level, msg: ClientGrenadeThrowMessage) {
-            val entityType = msg.grenadeType.getEntity()
-            val entity = entityType.create(level) ?: return
-            entity.initialize(msg.ownerUuid, msg.position, msg.velocity)
-            ModLogger.info("Spawning ${msg.grenadeType} entity at ${msg.position} with velocity ${msg.velocity.length()} blocks per tick")
-            level.addFreshEntity(entity)
+            val removeItem = !player.isCreative
+            CSGrenadesAPI.spawnGrenade(player, msg.context, removeItem)
         }
 
         fun fromInputState(): ClientGrenadeThrowMessage? {
@@ -67,7 +32,9 @@ data class ClientGrenadeThrowMessage(
             if (item !is CounterStrikeGrenadeItem) return null
             val grenadeType = item.grenadeType
 
-            return ClientGrenadeThrowMessage(player.uuid, position, velocity, grenadeType)
+            val context = GrenadeSpawnContext(grenadeType, player.uuid, position, velocity)
+
+            return ClientGrenadeThrowMessage(context)
 
         }
     }
